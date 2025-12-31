0f53ad47ae04ea0f3a1eb4c15cab092eae6b3d98
Roland Knall <rknall@gmail.com>
2025-12-31 11:13:29 +0100

    refactor: Update type hints for revision identifiers and dependencies

--- alembic/versions/9546bc823832_add_currency_support.py
+++ alembic/versions/9546bc823832_add_currency_support.py
@@ -8,16 +8,17 @@ Create Date: 2025-12-30 11:53:52.399121
 
 """
 
-from typing import Sequence, Union
+from collections.abc import Sequence
 
 import sqlalchemy as sa
+
 from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "9546bc823832"
-down_revision: Union[str, None] = "52a4318c20ae"
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | None = "52a4318c20ae"
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:

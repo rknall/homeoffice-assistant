462bf7b8cc7864874a99090df73b6219c7ca385f
Roland Knall <rknall@gmail.com>
2025-12-31 16:29:42 +0100

    chore: Lint fixes for alembic migration

--- alembic/versions/12b058676b09_add_expense_is_private_flag.py
+++ alembic/versions/12b058676b09_add_expense_is_private_flag.py
@@ -5,17 +5,17 @@ Revises: a1b2c3d4e5f6
 Create Date: 2025-12-31 12:42:14.962492
 
 """
-from typing import Sequence, Union
+from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '12b058676b09'
-down_revision: Union[str, None] = 'a1b2c3d4e5f6'
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | None = 'a1b2c3d4e5f6'
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
